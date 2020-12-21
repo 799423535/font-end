8c69d4486605705b967a3cc3629fd4b107ffc04d
BadBoy <799423535@qq.com>
2020-12-21 22:22:31 +0800

    项目配置

--- dao/conn.js
+++ dao/conn.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var pool  = mysql.createPool({
   connectionLimit : 10,
-  host            : "http://150.158.7.205:888",
+  host            : 'http://150.158.7.205:3306',
   user            : 'vue03',
   password        : 'root',
   database        : 'vue03'
