43eed355ef7b26ad6275ca0b820ffcb9d653ad96
BadBoy <799423535@qq.com>
2020-12-21 15:41:56 +0800

    后端初始化

---
+++ dao/conn.js
@@ -0,0 +1,9 @@
+var mysql = require('mysql');
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : http://150.158.7.205
+  user            : 'vue03',
+  password        : 'root',
+  database        : 'vue03'
+});
+module.exports=pool;
