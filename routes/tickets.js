b8f6885a79253db55790eb06436c703fdfbeb0f9
BadBoy <799423535@qq.com>
2020-12-22 17:25:20 +0800

    改端口

--- routes/tickets.js
+++ routes/tickets.js
@@ -3,6 +3,7 @@ const Tickets = require('../model/tickets');
 var router = express.Router();
 /* 获取所有的tickets */
 router.get('/getTickets', function (req, res, next) {
+    console.log(111);
     Tickets.find({}, function (err, results) {
         res.json({
             status: 0,
